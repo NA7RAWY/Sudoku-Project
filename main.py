839b51327c29b441582caf9d565e74f9ab4e8b4e
Mostafa Hussein <mostafa135679@gmail.com>
2024-12-01 01:23:07 +0200

    gen

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from backtracking_solver import solve_sudoku
 from genetic_solver import genetic_algorithm
 
 
@@ -10,17 +9,17 @@ def print_board(board):
 
 
 # Example Sudoku puzzle
-puzzle = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+puzzle=[
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
 
 if __name__ == "__main__":
     print("Original Puzzle:")
@@ -29,17 +28,14 @@ if __name__ == "__main__":
     # Solve using Backtracking Algorithm
     print("Solving with Backtracking Algorithm...")
     puzzle_copy = [row[:] for row in puzzle]  # Copy the puzzle to avoid modifying the original
-    if solve_sudoku(puzzle_copy):
-        print("Solved Puzzle:")
-        print_board(puzzle_copy)
-    else:
-        print("No solution exists using Backtracking Algorithm.")
+
 
     # Solve using Genetic Algorithm
-    print("Solving with Genetic Algorithm...")
-    solution = genetic_algorithm(puzzle)
-    if solution:
+print("Solving with Genetic Algorithm...")
+solution = genetic_algorithm(puzzle, n=len(puzzle))
+
+if solution:
         print("Solved Puzzle:")
         print_board(solution)
-    else:
+else:
         print("No solution found using Genetic Algorithm.")
